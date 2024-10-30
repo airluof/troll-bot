0e10f951a7008f527d53b7ecf59bc3c931c61a46
airluof <etofleks@gmail.com>
2024-10-30 15:17:27 +0300

    Update __main__.py

--- troll_bot/__main__.py
+++ troll_bot/__main__.py
@@ -2,13 +2,4 @@ import asyncio
 from troll_bot.main import main
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    
-    if loop.is_running():
-        # Если цикл уже запущен, создаем задачу для main
-        loop.create_task(main())
-    else:
-        try:
-            asyncio.run(main())
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    asyncio.run(main())
