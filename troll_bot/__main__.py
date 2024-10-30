5374efc9554f7a63e901c924297b7bec0ca5d508
airluof <etofleks@gmail.com>
2024-10-30 14:16:35 +0300

    Update __main__.py

--- troll_bot/__main__.py
+++ troll_bot/__main__.py
@@ -1,37 +1,6 @@
 import asyncio
-import logging
-import sys
-
-from docopt import docopt
-from troll_bot.run import run_bot_service
-
-log = logging.getLogger(__name__)
-console_handler = logging.StreamHandler(sys.stderr)
-
-async def main():
-    """Troll Bot - Annoy your friends with this Telegram Bot.
-    Usage:
-      troll-bot [options]
-      troll-bot (-h | --help)
-
-    Options:
-      --verbose     Show more output
-    """
-    setup_logging()
-    arguments = docopt(main.__doc__)
-    setup_console_handler(console_handler, arguments.get('--verbose'))
-    await run_bot_service()  # Вызов асинхронной функции с await
-
-def setup_logging():
-    root_logger = logging.getLogger()
-    root_logger.addHandler(console_handler)
-    root_logger.setLevel(logging.DEBUG)
-
-def setup_console_handler(handler, verbose):
-    if verbose:
-        handler.setLevel(logging.DEBUG)
-    else:
-        handler.setLevel(logging.INFO)
+from troll_bot.main import main
 
 if __name__ == "__main__":
-    asyncio.run(main())  # Запускаем асинхронную функцию main
+    # Запускаем асинхронную функцию main
+    asyncio.run(main())
