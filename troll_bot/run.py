9ced83e77d2c9d023ca3b196c39d58a06c58dd1b
airluof <etofleks@gmail.com>
2024-10-30 13:55:11 +0300

    Update run.py

--- troll_bot/run.py
+++ troll_bot/run.py
@@ -1,49 +1,37 @@
 import logging
 import os
-import time
-
-from telegram.ext import Updater
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
 from troll_bot import CERTIFICATE_PATH, BOT_URL
 from troll_bot.handler import get_update_handler, get_forward_handler, get_help_handler
 from troll_bot.utils import generate_random_string
 
-
 log = logging.getLogger(__name__)
 
-
-def run_bot_service():
+async def run_bot_service():
     token = os.environ['BOT_TOKEN']
-    updater = Updater(token, workers=10)
+    application = ApplicationBuilder().token(token).build()
 
-    updater.dispatcher.add_handler(get_update_handler())
-    updater.dispatcher.add_handler(get_forward_handler())
-    updater.dispatcher.add_handler(get_help_handler())
+    # Добавляем обработчики
+    application.add_handler(get_update_handler())
+    application.add_handler(get_forward_handler())
+    application.add_handler(get_help_handler())
 
     if BOT_URL:
         webhook_path = generate_random_string(length=20)
         webhook_uri = '/' + webhook_path
-        set_webhook(updater, webhook_uri)
-        updater.start_webhook('0.0.0.0', 5000, webhook_path)
+        await set_webhook(application, webhook_uri)  # Обратите внимание на await
+        await application.run_webhook(listen='0.0.0.0', port=5000, path=webhook_uri)
     else:
-        updater.start_polling(poll_interval=0.1, timeout=10)
+        await application.run_polling(poll_interval=0.1)
 
-    running = True
-    while running:
-        try:
-            time.sleep(20000)
-        except KeyboardInterrupt:
-            running = False
-    updater.stop()
-
-
-def set_webhook(updater, webhook_uri):
+async def set_webhook(application, webhook_uri):
     base_url = BOT_URL
     webhook_url = base_url + webhook_uri
     log.info('Setting URL: %s', webhook_url)
 
     if CERTIFICATE_PATH:
-        updater.bot.setWebhook(webhook_url, open(CERTIFICATE_PATH, 'rb'))
+        with open(CERTIFICATE_PATH, 'rb') as certificate:
+            await application.bot.setWebhook(webhook_url, certificate)
     else:
-        updater.bot.setWebhook(webhook_url)
-
+        await application.bot.setWebhook(webhook_url)
