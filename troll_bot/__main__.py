e4f895b4a03abed2925682cd24040124a53bfbad
airluof <etofleks@gmail.com>
2024-10-30 18:22:41 +0300

    Update __main__.py

--- troll_bot/__main__.py
+++ troll_bot/__main__.py
@@ -2,15 +2,20 @@ import logging
 import asyncio
 from troll_bot.run import run_bot_service
 
+async def main():
+    await run_bot_service()
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    
-    # Запуск асинхронной функции в текущем цикле событий
+
     try:
-        asyncio.run(run_bot_service())
-    except RuntimeError as e:
-        if str(e) == "This event loop is already running":
-            loop = asyncio.get_event_loop()
-            loop.create_task(run_bot_service())  # Запускаем функцию в уже запущенном цикле
+        # Проверяем, есть ли уже запущенный цикл событий
+        loop = asyncio.get_event_loop()
+        if loop.is_running():
+            # Если цикл событий уже запущен, создаем задачу
+            asyncio.ensure_future(main())
         else:
-            raise e
+            # Если цикл событий не запущен, запускаем его
+            loop.run_until_complete(main())
+    except Exception as e:
+        logging.error(f"Ошибка при запуске: {e}")
