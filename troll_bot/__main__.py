d418b471eca1d0a30582495b7f4758e540257ae6
airluof <etofleks@gmail.com>
2024-10-30 14:20:52 +0300

    Update __main__.py

--- troll_bot/__main__.py
+++ troll_bot/__main__.py
@@ -2,4 +2,11 @@ import asyncio
 from troll_bot.main import main
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    try:
+        asyncio.run(main())  # Попробуем запустить с помощью asyncio.run()
+    except RuntimeError as e:
+        if "This event loop is already running" in str(e):
+            loop = asyncio.get_event_loop()
+            loop.create_task(main())  # Запускаем асинхронную задачу, если цикл уже запущен
+        else:
+            raise
