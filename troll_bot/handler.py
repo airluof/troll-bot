29d09d6221f59204716ed954de41e455dedf0e53
airluof <etofleks@gmail.com>
2024-10-30 20:00:54 +0300

    Update handler.py

--- troll_bot/handler.py
+++ troll_bot/handler.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import Update
-from telegram.ext import CommandHandler, MessageHandler, Filters, ContextTypes
+from telegram.ext import CommandHandler, MessageHandler, filters, ContextTypes
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -8,14 +8,13 @@ log = logging.getLogger(__name__)
 
 async def reply_text(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     log.info('Received update: %s', update)
-    # Основная логика обработки текста
     await update.message.reply_text("Привет! Я бот.")
 
 def get_update_handler():
     return CommandHandler("update", reply_text)  # Обработчик для команды /update
 
 def get_forward_handler():
-    return MessageHandler(Filters.forwarded, reply_text)  # Обработчик для пересланных сообщений
+    return MessageHandler(filters.FORWARDED, reply_text)  # Обработчик для пересланных сообщений
 
 def get_help_handler():
     return CommandHandler("help", reply_text)  # Обработчик для команды /help
