2e650022247b9f98fc71b14aea35e841a7daa092
airluof <etofleks@gmail.com>
2024-10-30 13:58:37 +0300

    Update handler.py

--- troll_bot/handler.py
+++ troll_bot/handler.py
@@ -1,8 +1,5 @@
 import logging
-
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
-from telegram.ext import MessageHandler, CommandHandler, Filters
-
 from troll_bot.database import save_message
 from troll_bot.reply import (get_random_message_word, get_reply_message, should_reply, get_reply_type,
                              reply_text_message, reply_audio_message, reply_gif_message)
@@ -10,31 +7,29 @@ from troll_bot.utils import random_item
 
 log = logging.getLogger(__name__)
 
-
 def get_update_handler():
-    return MessageHandler(Filters.text, reply_text)
+    return MessageHandler(filters.TEXT, reply_text)
 
 def get_forward_handler():
-    return CommandHandler("fwd", forward_message, pass_args=True)
+    return CommandHandler("fwd", forward_message)
 
 def get_help_handler():
     return CommandHandler("help", print_help)
 
-@run_async
-def reply_text(bot, received, **kwargs):
-    log.debug('Received: %s', received)
+async def reply_text(update, context):
+    log.debug('Received: %s', update)
 
-    save_message(received.message)
+    save_message(update.message)
 
-    if not hasattr(received.message, 'text'):
+    if not hasattr(update.message, 'text'):
         log.info('Not text message received.')
         return
 
     if not should_reply():
         return
 
-    random_word = get_random_message_word(received.message)
-    reply_message = get_reply_message(random_word, received.message.chat.id)
+    random_word = get_random_message_word(update.message)
+    reply_message = get_reply_message(random_word, update.message.chat.id)
     if not reply_message:
         log.info('Not reply message')
         return
@@ -42,30 +37,29 @@ def reply_text(bot, received, **kwargs):
     reply_type = get_reply_type()
 
     if reply_type == 'text':
-        reply_text_message(bot, received.message.chat.id, reply_message)
+        await reply_text_message(context.bot, update.message.chat.id, reply_message)
 
     if reply_type == 'audio':
-        reply_audio_message(bot, received.message.chat.id, reply_message)
+        await reply_audio_message(context.bot, update.message.chat.id, reply_message)
 
     if reply_type == 'gif':
-        reply_gif_message(bot, received.message.chat.id, random_word)
+        await reply_gif_message(context.bot, update.message.chat.id, random_word)
 
-@run_async
-def forward_message(bot, update, args):
+async def forward_message(update, context):
+    args = context.args
     if not args:
         log.info('No args passed')
         return
 
-    reply_message = get_reply_message(args, update.message.chat.id)
+    reply_message = get_reply_message(args[0], update.message.chat.id)
     if not reply_message:
         log.info('Not reply message')
         return
 
-    reply_text_message(bot, update.message.chat.id, reply_message)
+    await reply_text_message(context.bot, update.message.chat.id, reply_message)
 
-@run_async
-def print_help(bot, update):
-    update.message.reply_text('''
+async def print_help(update, context):
+    await update.message.reply_text('''
         /fwd <text> : return some random message with words in <text>
         '''
-        )
+    )
