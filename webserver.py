f97fd8911673380bd6f153b5b475345e3199ffa8
airluof <etofleks@gmail.com>
2024-10-30 16:11:29 +0300

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,16 +1,22 @@
 from flask import Flask, request
+import logging
 
 app = Flask(__name__)
 
-@app.route('/b1871c694b036ae3097c5bf27f8733fcd33d6a1a', methods=['POST'])
-def handle_webhook():
-    # Здесь можно обрабатывать полученные данные от Telegram
+# Настраиваем логирование
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+@app.route('/<path:webhook_path>', methods=['POST'])
+def handle_webhook(webhook_path):
     data = request.get_json()
-    print("Получены данные от вебхука:", data)  # Выводим данные в консоль для проверки
+    log.info("Получены данные от вебхука: %s", data)  # Логируем полученные данные
+    log.info("Тип данных: %s", type(data))  # Логируем тип данных
+    # Здесь вы можете добавить дополнительную логику обработки данных
     return 'Webhook received', 200
 
 def keep_alive():
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)  # Запускаем Flask-сервер на всех интерфейсах
 
 if __name__ == "__main__":
-    keep_alive()
+    keep_alive()  # Запускаем Flask-сервер для поддержания активности
