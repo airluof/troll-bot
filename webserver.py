b429f249dbfb3d6079369d1c4b47d54f537a9412
airluof <etofleks@gmail.com>
2024-10-30 16:12:27 +0300

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -9,14 +9,14 @@ log = logging.getLogger(__name__)
 
 @app.route('/<path:webhook_path>', methods=['POST'])
 def handle_webhook(webhook_path):
+    log.info("Webhook path: %s", webhook_path)  # Логируем путь вебхука
     data = request.get_json()
     log.info("Получены данные от вебхука: %s", data)  # Логируем полученные данные
-    log.info("Тип данных: %s", type(data))  # Логируем тип данных
-    # Здесь вы можете добавить дополнительную логику обработки данных
+    if data:
+        log.info("Данные успешно получены.")
+    else:
+        log.warning("Получены пустые данные.")
     return 'Webhook received', 200
 
 def keep_alive():
-    app.run(host='0.0.0.0', port=5000)  # Запускаем Flask-сервер на всех интерфейсах
-
-if __name__ == "__main__":
-    keep_alive()  # Запускаем Flask-сервер для поддержания активности
+    app.run(host='0.0.0.0', port=5000)  # Запуск Flask-сервера на всех адресах
