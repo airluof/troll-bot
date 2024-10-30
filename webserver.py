9733365bd8a6064cbc8a78da9507e90743f4e3d8
airluof <etofleks@gmail.com>
2024-10-30 16:06:55 +0300

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,17 +1,16 @@
-from flask import Flask
-import os
-import threading
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Bot is alive!"
-
-def run():
-    port = int(os.environ.get("PORT", 5000))  # Используйте PORT из окружения
-    app.run(host="0.0.0.0", port=port)  # Слушаем на всех интерфейсах
+@app.route('/b1871c694b036ae3097c5bf27f8733fcd33d6a1a', methods=['POST'])
+def handle_webhook():
+    # Здесь можно обрабатывать полученные данные от Telegram
+    data = request.get_json()
+    print("Получены данные от вебхука:", data)  # Выводим данные в консоль для проверки
+    return 'Webhook received', 200
 
 def keep_alive():
-    t = threading.Thread(target=run)
-    t.start()
+    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    keep_alive()
