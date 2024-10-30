49e9ac85a4a911e02b8bdf4f7a846e3631bde4eb
airluof <etofleks@gmail.com>
2024-10-30 15:22:19 +0300

    Update __main__.py

--- troll_bot/__main__.py
+++ troll_bot/__main__.py
@@ -1,5 +1,7 @@
 import asyncio
-from troll_bot.main import main
+from troll_bot.run import run_bot_service
+from webserver import keep_alive  # Запуск Flask-сервера
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    keep_alive()  # Запускаем Flask-сервер
+    asyncio.run(run_bot_service())  # Запускаем бота
