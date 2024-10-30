5515a50531f25b108d493e5845325198f686cc4b
airluof <etofleks@gmail.com>
2024-10-30 16:48:44 +0300

    Update main.py

--- troll_bot/main.py
+++ troll_bot/main.py
@@ -3,7 +3,6 @@ import os
 import asyncio
 from telegram.ext import ApplicationBuilder
 from webserver import keep_alive  # Импортируем функцию для запуска Flask-сервера
-
 from troll_bot import CERTIFICATE_PATH, BOT_URL
 from troll_bot.handler import get_update_handler, get_forward_handler, get_help_handler
 from troll_bot.utils import generate_random_string
@@ -15,45 +14,41 @@ log = logging.getLogger(__name__)
 async def run_bot_service():
     token = os.getenv('BOT_TOKEN')  # Получаем токен бота из переменных окружения
     if not token:
-        log.error("BOT_TOKEN не найден. Убедитесь, что переменная окружения настроена.")
+        log.error("BOT_TOKEN не найден.")
         return
 
     application = ApplicationBuilder().token(token).build()
 
     # Добавляем обработчики
-    application.add_handler(get_update_handler())
-    application.add_handler(get_forward_handler())
-    application.add_handler(get_help_handler())
+    application.add_handler(get_update_handler())  # Обработчик текстовых сообщений
+    application.add_handler(get_forward_handler())  # Обработчик команды /fwd
+    application.add_handler(get_help_handler())      # Обработчик команды /help
 
+    # Запускаем бота в режиме вебхука или опроса
     if BOT_URL:
         webhook_path = generate_random_string(length=20)  # Генерируем случайный путь для вебхука
         webhook_uri = '/' + webhook_path
         await set_webhook(application, webhook_uri)  # Устанавливаем вебхук
-        await check_webhook(application)  # Проверка установленного вебхука
         port = int(os.environ.get("PORT", 5000))
         log.info(f"Запуск в режиме вебхука на порту {port} с URL: {BOT_URL}")
         await application.run_webhook(listen='0.0.0.0', port=port, path=webhook_uri)
     else:
-        log.info("Запуск в режиме опроса (polling)")
+        log.info("Запуск в режиме опроса")
         await application.run_polling(poll_interval=0.1)  # Запускаем бота в режиме опроса
 
 async def set_webhook(application, webhook_uri):
     base_url = BOT_URL
     webhook_url = base_url + webhook_uri
-    log.info('Установка вебхука по URL: %s', webhook_url)  # Логируем установку вебхука
+    log.info('Установка вебхука по URL: %s', webhook_url)
 
     try:
         if CERTIFICATE_PATH:
             with open(CERTIFICATE_PATH, 'rb') as certificate:
-                await application.bot.setWebhook(webhook_url, certificate=certificate)  # Устанавливаем вебхука с сертификатом
+                await application.bot.setWebhook(webhook_url, certificate=certificate)
         else:
-            await application.bot.setWebhook(webhook_url)  # Устанавливаем вебхука без сертификата
+            await application.bot.setWebhook(webhook_url)
     except Exception as e:
         log.error(f"Ошибка при установке вебхука: {e}")
-
-async def check_webhook(application):
-    webhook_info = await application.bot.getWebhookInfo()
-    log.info("Текущая информация о вебхуке: %s", webhook_info)  # Логируем информацию о вебхуке
 
 if __name__ == "__main__":
     keep_alive()  # Запускаем Flask-сервер для поддержания активности
