6bdc3614bd56bd0b59b894ff28799fb0ba99a797
airluof <etofleks@gmail.com>
2024-10-30 18:12:26 +0300

    Update __main__.py

--- troll_bot/__main__.py
+++ troll_bot/__main__.py
@@ -1,20 +1,16 @@
+import logging
 import asyncio
 from troll_bot.run import run_bot_service
-from webserver import keep_alive  # Импортируем функцию для запуска Flask-сервера
 
 if __name__ == "__main__":
-    keep_alive()  # Запускаем Flask-сервер
-
+    logging.basicConfig(level=logging.INFO)
+    
+    # Запуск асинхронной функции в текущем цикле событий
     try:
-        # Проверяем, есть ли уже активный event loop
-        loop = asyncio.get_running_loop()
-    except RuntimeError:
-        # Если нет активного event loop, создаем его
-        loop = None
-
-    if loop and loop.is_running():
-        # Если цикл уже работает, создаем задачу для run_bot_service
-        loop.create_task(run_bot_service())
-    else:
-        # Если нет активного цикла, создаем и запускаем новый
         asyncio.run(run_bot_service())
+    except RuntimeError as e:
+        if str(e) == "This event loop is already running":
+            loop = asyncio.get_event_loop()
+            loop.create_task(run_bot_service())  # Запускаем функцию в уже запущенном цикле
+        else:
+            raise e
