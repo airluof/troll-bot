580786150cdf305f2f2b4f5b784a73c405dcd1ad
airluof <etofleks@gmail.com>
2024-10-30 17:56:19 +0300

    Update run.py

--- troll_bot/run.py
+++ troll_bot/run.py
@@ -1,8 +1,6 @@
 import logging
 import os
-import asyncio
-from flask import Flask
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder
 
 from troll_bot import CERTIFICATE_PATH, BOT_URL
 from troll_bot.handler import get_update_handler, get_forward_handler, get_help_handler
@@ -11,9 +9,6 @@ from troll_bot.utils import generate_random_string
 # Настройка логгирования
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
-
-# Создаем Flask приложение
-app = Flask(__name__)
 
 async def run_bot_service():
     token = os.environ['BOT_TOKEN']  # Получаем токен из переменной окружения
@@ -27,11 +22,9 @@ async def run_bot_service():
     if BOT_URL:
         webhook_path = generate_random_string(length=20)
         webhook_uri = '/' + webhook_path
-        await set_webhook(application, webhook_uri)
-        
-        # Используем PORT из переменной окружения
+        await set_webhook(application, webhook_uri)  # Установка вебхука
         port = int(os.environ.get('PORT', 5000))
-        await application.run_webhook(listen='0.0.0.0', port=port, path=webhook_uri)
+        await application.run_webhook(listen='0.0.0.0', port=port)  # Убираем параметр path
     else:
         await application.run_polling(poll_interval=0.1)
 
@@ -46,16 +39,6 @@ async def set_webhook(application, webhook_uri):
     else:
         await application.bot.setWebhook(webhook_url)
 
-@app.route('/')
-def index():
-    return 'Bot is running!'
-
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    
-    # Запуск бота в отдельном потоке
-    loop.create_task(run_bot_service())
-    
-    # Запускаем Flask приложение через Uvicorn
-    import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    import asyncio
+    asyncio.run(run_bot_service())
