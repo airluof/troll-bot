88cae5d768b5d31704aa0505b45400e689b35256
airluof <etofleks@gmail.com>
2024-10-30 18:28:46 +0300

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,15 +1,22 @@
-from flask import Flask
-from threading import Thread
+from flask import Flask, request
+import logging
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Bot is running!"  # Простой ответ на корневой маршрут
+# Настраиваем логирование
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
-def run():
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/<path:webhook_path>', methods=['POST'])
+def handle_webhook(webhook_path):
+    log.info("Webhook path: %s", webhook_path)  # Логируем путь вебхука
+    data = request.get_json()
+    log.info("Получены данные от вебхука: %s", data)  # Логируем полученные данные
+    if data:
+        log.info("Данные успешно получены.")
+    else:
+        log.warning("Получены пустые данные.")
+    return 'Webhook received', 200
 
 def keep_alive():
-    thread = Thread(target=run)
-    thread.start()
+    app.run(host='0.0.0.0', port=5000)  # Запуск Flask-сервера на всех адресах
