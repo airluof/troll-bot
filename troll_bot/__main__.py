afd32fe74f12486e97f397b64b5e81480fabbb5e
airluof <etofleks@gmail.com>
2024-10-31 13:56:49 +0300

    Update __main__.py

--- troll_bot/__main__.py
+++ troll_bot/__main__.py
@@ -3,18 +3,7 @@ from troll_bot.run import run_bot_service
 from webserver import keep_alive  # Импортируем функцию для запуска Flask-сервера
 
 if __name__ == "__main__":
-    keep_alive()  # Запускаем Flask-сервер
-
-    try:
-        # Проверяем, есть ли уже активный event loop
-        loop = asyncio.get_running_loop()
-    except RuntimeError:
-        # Если нет активного event loop, создаем его
-        loop = None
-
-    if loop and loop.is_running():
-        # Если цикл уже работает, создаем задачу для run_bot_service
-        loop.create_task(run_bot_service())
-    else:
-        # Если нет активного цикла, создаем и запускаем новый
-        asyncio.run(run_bot_service())
+    keep_alive()  # Запускаем Flask-сервер для поддержки вебхука
+    
+    # Создаем и запускаем асинхронный event loop для бота
+    asyncio.run(run_bot_service())  
